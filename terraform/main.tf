6d8b92edc59df81f80e99058160a21c901da1d00
shubhamdhole <111689624+shubhamdhole97@users.noreply.github.com>
2026-03-29 07:24:12 +0000

    22

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,9 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 data "aws_vpc" "default" {
   default = true
 }
 
 resource "aws_instance" "example" {
-  ami             = data.aws_ami.ubuntu.id
+  ami             = "ami-05d2d839d4f73aafb"
   instance_type   = "t3.medium"
   key_name        = "pem.pem"
   security_groups = ["default"]
