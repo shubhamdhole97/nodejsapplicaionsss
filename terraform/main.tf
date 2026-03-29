b0aacae4d69b5a0726683e550403a8c811f7ab87
shubhamdhole <111689624+shubhamdhole97@users.noreply.github.com>
2026-03-29 07:26:47 +0000

    33

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,94 @@
+# Use default VPC (ap-south-1 default VPC) and default subnet
 data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_subnet_ids" "default_vpc" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_security_group" "instance" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-default-sg"
+  }
+}
+
 resource "aws_instance" "example" {
-  ami             = "ami-05d2d839d4f73aafb"
-  instance_type   = "t3.medium"
-  key_name        = "pem.pem"
-  security_groups = ["default"]
-  count           = 1
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  key_name               = "pem.pem"
+  subnet_id              = data.aws_subnet_ids.default_vpc.ids[0]
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  associate_public_ip_address = true
+  count                  = 1
 
   tags = {
     Name = "Ubuntu-24-LTS"
   }
+}
+
+output "instance_id" {
+  description = "Created EC2 instance ID"
+  value       = aws_instance.example[0].id
+}
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.example[0].public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.example[0].public_dns
 }
